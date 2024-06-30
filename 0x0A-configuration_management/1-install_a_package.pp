06fa3a9fcfe22ef05ca05fab2a40b5dddbd1daca
mo7amed3mad <memo12317emad@gmail.com>
2024-06-30 22:09:49 +0200

    pip

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,14 @@
+node default {
+  
+  # Ensure pip3 is installed
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  # Install Flask 2.1.0 using pip3
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+    require => Package['python3-pip'],
+  }
+}
