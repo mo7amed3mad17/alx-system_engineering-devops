6105b101407da876652f6997c0bbaeb34a0cf1da
mo7amed3mad <memo12317emad@gmail.com>
2024-08-14 13:34:55 +0200

    Top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Queries the reddit api.
+We are writting a function that queries the reddit api.
 and prints the titles of the first 10 hot post listed
 """
 
@@ -10,10 +10,10 @@ from sys import argv
 
 
 def top_ten(subreddit):
-    """Top 10 posts"""
+    """Return the top 10 posts"""
     user = {'User-Agent': 'Ntokozo'}
     url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                      .format(subreddit), headers = user).json()
+                       .format(subreddit), headers=user).json()
     try:
         for post in url.get('data').get('children'):
             print(post.get('data').get('title'))
